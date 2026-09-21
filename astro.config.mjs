70fa47e681e788bd732013243f28b922717afd4a
jayC02 <98602035+jayC02@users.noreply.github.com>
2026-09-21 14:55:47 +0100

    Enable React integration for React Bits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://www.scottishenergyservices.co.uk',
-  output: 'static'
+  output: 'static',
+  integrations: [react()]
 });
